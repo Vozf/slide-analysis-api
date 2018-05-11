c9415fb63b5225daf733c19c201cd396172f4813
Alexander Yaroshevich <vozman@yandex.ru>
2018-05-12 00:09:10 +0300

    update requirements and create method for similar map and read region

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # export PYTHONPATH="${PYTHONPATH}:/home/vozman/projects/slides/slide-analysis-service"
+# export PYTHONPATH="${PYTHONPATH}:/home/vozman/projects/slides/slide_analysis_nn"
 
-from collections import OrderedDict
-from flask import Flask, abort, make_response, jsonify, request
-from io import BytesIO
+from collections import (
+    OrderedDict,
+    namedtuple,
+)
+
+from PIL import Image
+from flask import Flask, abort, make_response, jsonify, request, send_file, send_from_directory
+from io import BytesIO, StringIO
 from openslide import OpenSlide, OpenSlideError
 from openslide.deepzoom import DeepZoomGenerator
 import os
 from optparse import OptionParser
 from threading import Lock
 from slide_analysis_service.interface import SlideAnalysisService
+# from slide_analysis_nn.prediction import Predict
+
+from constants import SIMILARITY_MAP_PATH
 
 SLIDE_DIR = '/home/vozman/Pictures/'
 SLIDE_CACHE_SIZE = 10
@@ -16,6 +25,8 @@ DEEPZOOM_FORMAT = 'jpeg'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+SlideObject = namedtuple('SlideObject', ['openslide', 'deepzoom', 'path'])
 
 
 class _SlideCache:
@@ -28,19 +39,20 @@ class _SlideCache:
         with self._lock:
             if path in self._cache:
                 # Move to end of LRU
-                slide = self._cache.pop(path)
-                self._cache[path] = slide
-                return slide
+                slide_object = self._cache.pop(path)
+                self._cache[path] = slide_object
+                return slide_object
 
-        osr = OpenSlide(path)
-        slide = DeepZoomGenerator(osr)
+        openslide = OpenSlide(path)
+        deepzoom = DeepZoomGenerator(openslide)
+        slide_object = SlideObject(openslide=openslide, deepzoom=deepzoom, path=path)
 
         with self._lock:
             if path not in self._cache:
                 if len(self._cache) == self.cache_size:
                     self._cache.popitem(last=False)
-                self._cache[path] = slide
-        return slide
+                self._cache[path] = slide_object
+        return slide_object
 
 
 def _get_slides(basedir, relpath=''):
@@ -53,8 +65,7 @@ def _get_slides(basedir, relpath=''):
             if cur_dir:
                 children.append(cur_dir)
         elif OpenSlide.detect_format(cur_path):
-            children.append({"name": os.path.basename(cur_path), "url_path": cur_path})
-
+            children.append({"name": os.path.basename(cur_path)})
     return children
 
 
@@ -82,7 +93,6 @@ def _get_slide(path):
         abort(404)
     try:
         slide = app.cache.get(path)
-        slide.filename = os.path.basename(path)
         return slide
     except OpenSlideError:
         abort(404)
@@ -93,16 +103,37 @@ def index():
     return jsonify(_get_slides(app.basedir))
 
 
-@app.route('/image/<path:path>')
+@app.route('/images/<path:filename>/preview')
+def get_preview(filename):
+    img_io = BytesIO()
+    osr = OpenSlide(SLIDE_DIR + '/' + filename)
+    osr.get_thumbnail((500, 500)).save(img_io, 'JPEG')
+    resp = make_response(img_io.getvalue())
+    resp.mimetype = 'image/jpg'
+    return resp
+
+@app.route('/images/<path:filename>/properties')
+def get_properties(filename):
+    properties = _get_slide(filename).openslide.properties
+    return jsonify(dict(properties))
+
+
+@app.route('/images/<path:filename>/dimensions')
+def get_dimensions(filename):
+    dimensions = _get_slide(filename).openslide.dimensions
+    return jsonify({'x': dimensions[0], 'y': dimensions[1]})
+
+
+@app.route('/images/<path:path>')
 def dzi(path):
-    slide = _get_slide(path)
+    slide = _get_slide(path).deepzoom
     format = app.config['DEEPZOOM_FORMAT']
     resp = make_response(slide.get_dzi(format))
     resp.mimetype = 'application/xml'
     return resp
 
 
-@app.route('/image/<path:path>/similar', methods=['POST'])
+@app.route('/images/<path:path>/similar', methods=['POST'])
 def find_similar(path):
     body = request.get_json()
     img_path = os.path.abspath(os.path.join(app.basedir, path))
@@ -111,15 +142,41 @@ def find_similar(path):
     if not slide.is_ready():
         slide.precalculate()
 
+
     similar = slide.find((body["x"], body["y"], body["width"], body["height"]),
                          app.slide_analysis_service.get_similarities()[1]())
 
+    similar['sim_map'].save(SIMILARITY_MAP_PATH, 'PNG')
+
     return jsonify(similar["top_n"])
 
+@app.route('/images/<path:path>/similar/map')
+def get_similarity_map(path):
+    # TODO create map file for every find_similar request
+    buf = BytesIO()
+    map = Image.open(SIMILARITY_MAP_PATH)
+    map.save(buf, 'PNG')
+    resp = make_response(buf.getvalue())
+    resp.mimetype = 'image/png'
+    return resp
 
-@app.route('/image/<path:path>_files/<int:level>/<int:col>_<int:row>.<format>')
+
+@app.route('/images/<path:path>/read_region')
+def read_region(path):
+    slide = _get_slide(path).openslide
+    tile = slide.read_region((int(request.args.get('x')), int(request.args.get('y'))), 0,
+                             (int(request.args.get('width')), int(request.args.get('height'))))
+
+    buf = BytesIO()
+    tile.save(buf, 'PNG')
+    resp = make_response(buf.getvalue())
+    resp.mimetype = 'image/png'
+    return resp
+
+
+@app.route('/images/<path:path>_files/<int:level>/<int:col>_<int:row>.<format>')
 def tile(path, level, col, row, format):
-    slide = _get_slide(path)
+    slide = _get_slide(path).deepzoom
     format = format.lower()
     if format != 'jpeg' and format != 'png':
         # Not supported by Deep Zoom
